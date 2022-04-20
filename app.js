30d87b3619b4ea9223473fd42ffd96c2abbc0f01
Julie <jatkin80@ford.com>
2022-04-20 10:28:02 -0600

    add styling to main page

--- app.js
+++ app.js
@@ -1,52 +1,26 @@
     const baseUrl = 'https://api.nasa.gov/planetary/apod?api_key='
     const apiKey = 'o5gNtRw010GxSEnvZU9WrzxQzGSBGjGVy5AeNHcD'
+    const $title = document.querySelector('#title')
+    const $date = document.querySelector('#date')
+    const $picture = document.querySelector('#picture')
+    const $explanation = document.querySelector('#explanation')
 
-    const title = document.querySelector("#title");
-    const copyright = document.querySelector("#copyright");
-    const mediaSection = document.querySelector("#media-section");
-    const information = document.querySelector("#description");
-    const imageSection = `<a id="hdimg" href="" target="-blank">
-     <div class="image-div">
-     <img id="image_of_the_day" src="" alt="image-by-nasa">
-     </div>
-    </a>`
-
-    function fetchData() {
+    const fetchNASAData = async() => {
         try {
-            fetch((baseUrl + apiKey))
-                .then(response => response.json())
-                .then(json => {
-                    diplaydata(json)
-                })
+            const response = await fetch(`${baseUrl}${apiKey}`)
+            const data = await response.json()
+            console.log('NASA APOD data', data)
+            displayData(data)
         } catch (error) {
             console.log(error)
         }
     }
 
-    function diplaydata(data) {
-
-        title.innerHTML = data.title;
-
-        if (data.hasOwnProperty("copyright")) {
-            copyright.innerHTML = data.copyright;
-        } else {
-            copyright.innerHTML = ""
-        }
-
-
-        date.innerHTML = data.date;
-
-
-
-        if (data.media_type == "video") {
-            mediaSection.innerHTML = videoSection;
-            document.getElementById("videoLink").src = data.url;
-
-        } else {
-            mediaSection.innerHTML = imageSection;
-            document.getElementById("hdimg").href = data.hdurl;
-            document.getElementById("image_of_the_day").src = data.url;
-        }
-        information.innerHTML = data.explanation
+    const displayData = data => {
+        $title.textContent = data.title
+        $date.textContent = data.date
+        $picture.src = data.hdurl
+        $explanation.textContent = data.explanation
     }
-    fetchData();
+
+    fetchNASAData()
