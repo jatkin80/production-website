92ce4dca1faa0822558cacb99e6c7f4b66e78fb4
Julie <jatkin80@ford.com>
2022-04-21 15:55:44 -0600

    404 page

--- site/app.js
+++ site/app.js
@@ -1,35 +1,36 @@
 const apiKey = "o5gNtRw010GxSEnvZU9WrzxQzGSBGjGVy5AeNHcD"
-
-
 const $title = document.querySelector("#title")
 const $date = document.querySelector("#date")
 const $picture = document.querySelector("#picture")
 const $explanation = document.querySelector("#explanation")
 const form = document.querySelector("form")
-
+const button = document.querySelector("button")
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const formData = new FormData(event.target)
-  const newDate = formData.get("newDate")
-  console.log(newDate)
-
-  console.log(formData)
-
-  window.location.href = `date.html?newDate=${newDate}`
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const newDate = formData.get("newDate")
+    window.location.href = `date.html?newDate=${newDate}`
 })
 
-
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey }`)
-  .then(response => response.json())
-  .then(data => {
-    useApiData(data)
-  })
+    .then(response => response.json())
+    .then(data => {
+        useApiData(data)
+    })
+    .catch(error => {
+        h1.textContent = "Let's Get You Back on Solid Ground"
+        button.textContent = "Return Home"
+        console.error(error)
+    })
 
 
-function useApiData (data) {
-  $title.textContent = data.title
-  $date.textContent = data.date
-  $picture.src = data.hdurl
-  $explanation.textContent = data.explanation
+function useApiData(data) {
+    const currentDate = new Date().toISOString().slice(0, 10)
+    newDate.max = currentDate
+    newDate.min = "1995-06-16"
+    $title.textContent = data.title
+    $date.textContent = data.date
+    $picture.src = data.hdurl
+    $explanation.textContent = data.explanation
 }
--- site/date.js
+++ site/date.js
@@ -1,24 +1,30 @@
 const apiKey = "o5gNtRw010GxSEnvZU9WrzxQzGSBGjGVy5AeNHcD"
 const url = new URL(window.location)
-console.log(url)
 const queryString = new URLSearchParams(url.search)
-console.log(queryString)
 const newDate = `${queryString.get("newDate")}`
 const $title = document.querySelector("#title")
 const $date = document.querySelector("#date")
 const $picture = document.querySelector("#picture")
 const $explanation = document.querySelector("#explanation")
-
+const button = document.querySelector("button")
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${ apiKey }&date=${newDate}`)
-  .then(response => response.json())
-  .then(data => {
-    useApiData(data)
-  })
+    .then(response => response.json())
+    .then(data => {
+        useApiData(data)
+    })
+    .catch(error => {
+        h1.textContent = "Let's Get You Back on Solid Ground"
+        button.textContent = "Return Home"
+        console.error(error)
+    })
 
-function useApiData (data) {
-  $title.textContent = data.title
-  $date.textContent = data.date
-  $picture.src = data.hdurl
-  $explanation.textContent = data.explanation
+function useApiData(data) {
+    const currentDate = new Date().toISOString().slice(0, 10)
+    newDate.max = currentDate
+    newDate.min = "1995-06-16"
+    $title.textContent = data.title
+    $date.textContent = data.date
+    $picture.src = data.hdurl
+    $explanation.textContent = data.explanation
 }
